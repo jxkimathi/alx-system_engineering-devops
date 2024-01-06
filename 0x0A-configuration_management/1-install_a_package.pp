771b38fc8fb95b110c5db94f05d734891ee2a160
jude <123254079+jxkimathi@users.noreply.github.com>
2024-01-06 10:54:37 +0300

    correction

--- 0x0A-configuration_management/1-install_a_package.pp
+++
@@ -1,6 +0,0 @@
-# Installs flask version 2.1.0.
-
-package { 'flask':
-  ensure    => '2.1.0',
-  provider  => 'pip3',
-}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,8 +0,0 @@
-# Creates a manifest that kills a process killmenow
-
-exec { 'pkill killmenow':
-  path      => '/usr/bin',
-  command   => 'pkill killmenow',
-  provider  => shell,
-  returns   => [0, 1]
-}
