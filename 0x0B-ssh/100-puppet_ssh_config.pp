cd26a4f7b2b30aa63eb0948bd35797e9e1526bab
jxkimathi <judescriva8@gmail.com>
2024-01-21 20:19:11 +0300

    correction

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Using Puppet to make changes to our configuration file
+
+file { '/etc/ssh/ssh_config':
+  ensure  => present,
+  content => "host*
+    IdentityFile ~/.ssh/school
+    PasswordAuthentication no"
+}
