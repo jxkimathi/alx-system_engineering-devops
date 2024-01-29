5af123c8a491fe9f97d9374c388dcfc44892807d
jxkimathi <judescriva8@gmail.com>
2024-01-29 16:22:17 +0300

    puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,25 @@
+# Installs a Nginx server with custom HTTP header
+
+exec { 'update':
+  provider => shell,
+  command  => "sudo apt-get -y update",
+  before   => Exec['install Nginx'],
+}
+
+exec { 'install Nginx':
+  provider => shell,
+  command  => "sudo apt-get -y install nginx",
+  before   => Exec['add_header'],
+}
+
+exec { 'add_header':
+  provider    => shell,
+  environment => ["HOST=${HOSTNAME}"],
+  command     => 'sudo sed -i "server_name _/a add_header X-ServedBy $HOSTNAME;" /etc/nginx/sites-enabled/default',
+  before      => Exec['restart Nginx'], 
+}
+
+exec { 'restart Nginx':
+  provider => shell,
+  command  => "sudo service nginx restart",
+}
